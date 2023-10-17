711547583973994314fa27eacb8f01979bdc860a
Md Mosfequr <97782191+mosfequr11@users.noreply.github.com>
2023-10-17 08:38:09 +0600

    add: set and get local storage

---
+++ js/storage-api.js
@@ -0,0 +1,15 @@
+// The back() method loads the previous URL in the windows.history list.
+// It is the same as clicking the "back arrow" in your browser.
+function setLocalStorage(key, value) {
+  localStorage.setItem(key, value);
+}
+
+//The go() method loads a specific URL from the history list:
+function getLocalStorage(key) {
+  alert(localStorage.getItem(key));
+}
+
+//
+// removeLocalStorage(key){
+//   localStorage.removeItem(key);
+// }
