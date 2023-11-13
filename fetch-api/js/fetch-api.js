3ae53e4c3528a96191b6e3cc9cba18162a265d8f
Md Mosfequr <97782191+mosfequr11@users.noreply.github.com>
2023-11-14 02:09:28 +0600

    fix: get data from text file

--- fetch-api/js/fetch-api.js
+++ fetch-api/js/fetch-api.js
@@ -1,9 +1,9 @@
-
 var displayData = document.getElementById("display");
 
-getData(){
-fetch("file:///D:/hw/api/fetch-api/demo-data.txt").then(res => res.text());
-.then(data => {
-  displayData.innerText = data;
-})
+function getData() {
+  fetch("http://127.0.0.1:5500/fetch-api/demo-data.txt")
+    .then((res) => res.text())
+    .then((data) => {
+      displayData.innerText = data;
+    });
 }
